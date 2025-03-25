6b7fb0fb184a7cbdf2481ebdc13ac427ec7a2f0b
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-03-25 20:05:32 +0000

    add entity

---
+++ src/main/java/com/raxrot/springboot/entity/Product.java
@@ -0,0 +1,36 @@
+package com.raxrot.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "stock_keeping_unit",nullable = false,unique = true)
+    private String sku;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageUrl;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+}
