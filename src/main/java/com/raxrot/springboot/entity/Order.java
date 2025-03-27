7a5db609ab111db42a69540671b9dd255bb85354
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-03-27 20:25:49 +0000

    add one to many

--- src/main/java/com/raxrot/springboot/entity/Order.java
+++ src/main/java/com/raxrot/springboot/entity/Order.java
@@ -1,12 +1,14 @@
 package com.raxrot.springboot.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -26,7 +28,10 @@ public class Order {
     private LocalDateTime dateUpdated;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "billing_address_id",referencedColumnName = "id")
+    @JoinColumn(name = "billing_address_id")
     private Address billingAddress;
 
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id")
+    Set<OrderItem> orderItems = new HashSet<>();
 }
---
+++ src/main/java/com/raxrot/springboot/entity/OrderItem.java
@@ -0,0 +1,26 @@
+package com.raxrot.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String imageUrl;
+    private BigDecimal price;
+    private int quantity;
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
